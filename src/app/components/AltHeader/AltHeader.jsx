ffbaebde9687483026ec78e5c6b03318d979e30f
01joanna <joa.cuevas9@gmail.com>
2025-06-10 21:40:54 +0200

    prueba header

--- src/app/components/AltHeader/AltHeader.jsx
+++ src/app/components/AltHeader/AltHeader.jsx
@@ -1,25 +1,14 @@
 export default function AltHeader() {
     return (
-        <header
-            className="z-10 w-full h-auto relative">
-                <div className="bg-black">
-                    <nav className="">
-                        <ul className="flex px-10 py-1 justify-end gap-10 uppercase text-xs tracking-wider bg-transparent text-white">
-                            <li>
-                                <a href="/" className="">Home</a>
-                            </li>
-                            <li>
-                                <a href="/information" className="">Information</a>
-                            </li>
-                            <li>
-                                <a href="/projects" className="">Projects</a>
-                            </li>
-                            <li>
-                                <a href="/contact" className="">Contact</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+        <header className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-auto h-auto text-md font-oatmealLight uppercase tracking-wider flex mt-14 gap-64">
+            <nav className="">
+                <ul className="flex gap-20 justify-end border border-gray-400 rounded-lg px-7 py-2">
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/information">Información</a></li>
+                    <li><a href="/projects">Proyectos</a></li>
+                </ul>
+            </nav>
+            <div className="uppercase border border-gray-400 rounded-lg px-7 py-2">Contacto</div>
         </header>
     )
 }
