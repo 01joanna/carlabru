4257625a2ff2d0ef44d6a00c30b91237131966d2
01joanna <joa.cuevas9@gmail.com>
2025-06-10 21:47:52 +0200

    info

--- src/app/components/AltHeader/AltHeader.jsx
+++ src/app/components/AltHeader/AltHeader.jsx
@@ -1,6 +1,6 @@
 export default function AltHeader() {
     return (
-        <header className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-auto h-auto text-md font-oatmealLight uppercase tracking-wider flex mt-14 gap-64">
+        <header className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 w-auto h-auto text-md font-oatmealLight uppercase tracking-wider flex mt-14 gap-64 bg-transparent text-white">
             <nav className="">
                 <ul className="flex gap-20 justify-end border border-gray-400 rounded-lg px-7 py-2">
                     <li><a href="/">Inicio</a></li>
@@ -8,7 +8,9 @@ export default function AltHeader() {
                     <li><a href="/projects">Proyectos</a></li>
                 </ul>
             </nav>
-            <div className="uppercase border border-gray-400 rounded-lg px-7 py-2">Contacto</div>
+            <a
+            href="/contact"
+            className="uppercase border border-gray-400 rounded-lg px-7 py-2">Contacto</a>
         </header>
     )
 }
--- src/app/information/page.js
+++ src/app/information/page.js
@@ -1,10 +1,10 @@
 export default function Information() {
     return (
-        <section className="font-helvetica tracking-wider mx-30 my-20 text-sm w-full">
-            <div className="flex gap-[20rem]">
-                <div className="w-1/3 text-justify flex flex-col gap-20">
+        <section className="font-helvetica tracking-wider mx-30 my-36 text-sm w-full bg-black text-white">
+            <div className="flex gap-[10rem]">
+                <div className="w-1/3 flex flex-col gap-20 font-oatmealLight text-lg">
                     <div>
-                        Carla Bru Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis felis id imperdiet convallis. Nullam lacinia mattis tellus, sed eleifend mauris commodo sollicitudin. Aenean egestas venenatis enim. Ut leo sem, lobortis vitae odio ut, aliquet ornare nisl. Phasellus finibus lacus et mi viverra, in gravida dolor varius. Duis et est quam. Maecenas a sollicitudin enim, finibus imperdiet velit. Sed venenatis felis nec bibendum ultrices. Proin posuere consequat tellus non efficitur. Vivamus imperdiet ultrices mollis. Proin vitae bibendum ante. <br /> <br />Fusce rutrum tincidunt aliquam. Sed scelerisque vehicula urna, et laoreet sapien placerat quis. Nunc elit lorem, dictum vel erat efficitur, ornare porta eros. Nulla a dapibus turpis. Integer semper posuere sodales. Donec suscipit faucibus facilisis. Proin tempor semper ex, eu eleifend nisl scelerisque non. Maecenas metus nunc, fringilla eu est ut, fermentum feugiat augue. Nunc suscipit nunc vel dui pharetra sodales. Vestibulum tincidunt diam at turpis scelerisque vehicula. Vestibulum dui leo, cursus in massa tincidunt, iaculis interdum ligula. Mauris mattis fringilla ante.</div>
+                        Carla Bru Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis felis id imperdiet convallis. Nullam lacinia mattis tellus, sed eleifend mauris commodo sollicitudin. Aenean egestas venenatis enim. Ut leo sem, lobortis vitae odio ut, aliquet ornare nisl. <br /> <br />Fusce rutrum tincidunt aliquam. Sed scelerisque vehicula urna, et laoreet sapien placerat quis. Nunc elit lorem, dictum vel erat efficitur, ornare porta eros. Nulla a dapibus turpis. Integer semper posuere sodales. Donec suscipit faucibus facilisis. Proin tempor semper ex, eu eleifend nisl scelerisque non. </div>
                     <div>
 
                         <div className="grid grid-cols-2 grid-rows-2 gap-1 w-1/2">
@@ -17,9 +17,9 @@ export default function Information() {
                         </div>
                     </div>
                 </div>
-                <div className="w-1/2 flex flex-col gap-20">
+                <div className="w-1/2 flex flex-col gap-20 font-oatmealExtraLight text-lg">
                     <div className="flex flex-col gap-3">
-                        <b className="uppercase pb-6">Projects</b>
+                        <b className="uppercase pb-6 font-oatmealMedium">Projects</b>
                         <div className="flex flex-row gap-20">
                             <p>Año</p>
                             <p>Proyecto, producido por Lorem Ipsum</p>
@@ -36,7 +36,7 @@ export default function Information() {
 
                     </div>
                     <div>
-                        <b className="uppercase">Contact</b>
+                        <b className="uppercase font-oatmealRegular">Contact</b>
                         <p>Email</p>
                         <p>Instagram</p>
                         <p>Vimeo</p>
